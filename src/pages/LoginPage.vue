9bfbe1972815acab789b33b54b624a4c0e556725
Алишер <alisher@MacBook-Air-Aliser.local>
2023-06-03 19:45:53 +0500

    change: layout on LoginPage

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,15 +1,16 @@
 <template lang="pug">
-div(class="login-form")
-  h1 Авторизация
+div(class="page")
+  div(class="login-form")
+    h1 Авторизация
 
-  form(class="login-form__form")
-    input(v-model="formData.login" autofocus type="text" class="login-form__input input" placeholder="Логин")
-    input(v-model="formData.password" type="password" class="login-form__input input" placeholder="Пароль")
-    button(@click.enter="singIn" type="button" :disabled="!isFilled" class="login-form__btn btn btn--add") Войти
+    form(class="login-form__form")
+      input(v-model="formData.login" autofocus type="text" class="login-form__input input" placeholder="Логин")
+      input(v-model="formData.password" type="password" class="login-form__input input" placeholder="Пароль")
+      button(@click.enter="singIn" type="button" :disabled="!isFilled" class="login-form__btn btn btn--add") Войти
 
-  div(v-if="isAuthenticated" class="alert")
-    | Не верно указан логин или пароль. #[br] Небольшая подсказочка, логин: demo, пароль: demo
-    
+    div(v-if="isAuthenticated" class="alert")
+      | Не верно указан логин или пароль. #[br] Небольшая подсказочка, логин: demo, пароль: demo
+      
 </template>
 
 <script setup>
